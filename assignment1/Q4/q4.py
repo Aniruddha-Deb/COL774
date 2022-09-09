34ae4de509b0a38c5963c0812e44b925c3be28e0
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-09-09 18:15:22 +0530

    A1 done

--- assignment1/Q4/q4.py
+++ assignment1/Q4/q4.py
@@ -14,6 +14,9 @@ import sys
 def normalize(a):
     return (a-a.mean(axis=0))/a.std()
 
+def phi(Y):
+    return np.count_nonzero(Y!=0)/len(Y)
+
 def mu(X,Y):
     X1, X2 = X[Y==0],X[Y!=0]
     return np.array([X1.mean(axis=0),X2.mean(axis=0)])
@@ -46,12 +49,12 @@ def plot_data(X,Y,save_file_name=None):
 # part (c)
 #########################################
 
-def plot_boundary(X,Y,mu,save_file_name=None):
+def plot_boundary(X,Y,phi,mu,save_file_name=None):
     X_x, X_y = X.T
     s = plt.scatter(X_x,X_y,c=Y,cmap=ListedColormap(['red','green']))
     
     # getting separator: perp. bisector of mu's
-    midpoint = (mu[0] + mu[1])/2
+    midpoint = mu[0]*(1-phi) + mu[1]*phi
     diff = (mu[1]-mu[0])
     slope = -diff[1]/diff[0]
     print(midpoint,slope)
@@ -81,12 +84,13 @@ def Si(X,Y):
 # part (e)
 #########################################
 
-def boundary_fn(mu,det_sigma,inv_sigma,X):
+def boundary_fn(phi,mu,det_sigma,inv_sigma,X):
 
     return (X-mu[0]).T@inv_sigma[0]@(X-mu[0]) - (X-mu[1]).T@inv_sigma[1]@(X-mu[1]) \
-         + np.log(det_sigma[0]) - np.log(det_sigma[1])
+         + np.log(det_sigma[0]) - np.log(det_sigma[1]) \
+         + 2*np.log(phi/(1-phi))
 
-def plot_quadratic_boundary(X,Y,mu,sigma,save_file_name=None):
+def plot_quadratic_boundary(X,Y,phi,mu,sigma,save_file_name=None):
     X_x, X_y = X.T
     s = plt.scatter(X_x,X_y,c=Y,cmap=ListedColormap(['red','green']))
     
@@ -98,8 +102,8 @@ def plot_quadratic_boundary(X,Y,mu,sigma,save_file_name=None):
     x1_space = np.linspace(-1,1,801)
     x_meshgrid = np.stack(np.meshgrid(x0_space, x1_space),axis=2)
     
-    vector_boundary_fn = np.vectorize(boundary_fn, signature="(2,2),(2),(2,2,2),(2)->()")
-    boundary_2d = vector_boundary_fn(mu,det_sigma,inv_sigma,x_meshgrid)
+    vector_boundary_fn = np.vectorize(boundary_fn, signature="(),(2,2),(2),(2,2,2),(2)->()")
+    boundary_2d = vector_boundary_fn(phi,mu,det_sigma,inv_sigma,x_meshgrid)
     plot_pts = x_meshgrid[(boundary_2d<0.005)&(boundary_2d>-0.005)].T
     
     plt.plot(plot_pts[0], plot_pts[1], color='b')
@@ -119,18 +123,20 @@ class GaussianDiscriminantAnalyser:
     def __init__(self):
         self.mu = None
         self.sigma = None
+        self.phi = None
         self.det_sigma = None
         self.inv_sigma = None
-        self.bf = np.vectorize(boundary_fn, signature="(2,2),(2),(2,2,2),(2)->()")
+        self.bf = np.vectorize(boundary_fn, signature="(),(2,2),(2),(2,2,2),(2)->()")
     
     def fit(self,X,Y):
+        self.phi = phi(Y)
         self.mu = mu(X,Y)
         self.sigma = Si(X,Y)
         self.det_sigma = [np.linalg.det(self.sigma[0]), np.linalg.det(self.sigma[1])]
         self.inv_sigma = [np.linalg.inv(self.sigma[0]), np.linalg.inv(self.sigma[1])]
         
     def predict(self,X):
-        Y = self.bf(self.mu, self.det_sigma, self.inv_sigma, X)
+        Y = self.bf(self.phi, self.mu, self.det_sigma, self.inv_sigma, X)
         return np.where(Y>0,'Canada', 'Alaska')
 
 
