797d88357a607a53e31f9a86058dce4a6695574e
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-10-17 22:52:43 +0530

    Working on A3

--- assignment2/Q3/Qd/qd.py
+++ assignment2/Q3/Qd/qd.py
@@ -1,5 +1,5 @@
 from sklearn import svm
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,6 +21,17 @@ def load_data(dir, type):
     
     return data
 
+def cross_validate(model, data, labels, degree):
+    scores = []
+    kf = KFold(n_splits=degree, shuffle=True)
+    for train_idx, test_idx in kf.split(data):
+        model.fit(data[train_idx], labels[train_idx])
+        preds = model.predict(data[test_idx])
+        score = accuracy_score(labels[test_idx], preds)
+        scores.append(score)
+
+    return scores
+
 if __name__ == "__main__":
     train_data = load_data(sys.argv[1], 'train_data')
     test_data = load_data(sys.argv[2], 'test_data')
@@ -29,7 +40,7 @@ if __name__ == "__main__":
     cv_scores = []
     for c in C:
         clf = svm.SVC(gamma=0.001, C=c)
-        scores = cross_val_score(clf, train_data['data'], train_data['labels'], cv=5, scoring='accuracy', n_jobs=-1)
+        scores = cross_validate(clf, train_data['data'], train_data['labels'], 5)
         print(scores)
         cv_scores.append(scores)
 
